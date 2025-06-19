03d760e3f9e81e0618bd1f710198cee1537fe9ba
andresisfx <andresrodriguezwg@gmail.com>
2025-06-19 16:31:24 -0500

    animations to the hamburger menu

--- Eccomerce/JS/main.js
+++ Eccomerce/JS/main.js
@@ -1,6 +1,60 @@
-const hamburger=document.getElementById('hamburger');
-const menu=document.getElementById('navContenedor');
-console.log(hamburger,menu)
-hamburger.addEventListener('click',function(){
-    menu.classList.toggle('menu-active')
+// const hamburger=document.getElementById('hamburger');
+// const menu=document.getElementById('navContenedor');
+// console.log(hamburger,menu)
+// hamburger.addEventListener('click',function(){
+//     menu.classList.toggle('menu-active')
+// })
+
+const nav = document.getElementById('navContenedor');
+const botonMenu = document.getElementById('hamburger'); // Ajusta este selector
+
+// botonMenu.addEventListener("click", () => {
+//     if (nav.classList.contains("menu-active")) {
+//         // Cerrar menú (con transición)
+//         nav.style.transition = "opacity 0.3s ease, transform 0.3s ease";
+//         nav.style.opacity = "0";
+      
+//         // nav.style.transform = "translateY(-100%)";
+        
+//         // Esperar a que termine la transición antes de aplicar display: none
+//         setTimeout(() => {
+//             document.body.style.overflow='auto'
+//             nav.classList.remove("menu-active");
+//             nav.style.transition = ""; // Limpiar estilos en línea
+//         }, 500); // Tiempo igual a la duración de la transición (500ms)
+//     } else {
+//         // Abrir menú
+//         nav.classList.add("menu-active");
+//         document.body.style.overflow='hidden'
+//         // Esperar un frame para que el navegador registre el cambio antes de la animación
+//         requestAnimationFrame(() => {
+//             nav.style.transition = "height 0.5s ease, opacity 0.3s ease, transform 0.5s ease";
+//             nav.style.opacity = "1";
+//             nav.style.transform = "translateY(0)";
+//         });
+//     }
+// }); 
+
+botonMenu.addEventListener('click',function(){
+    if(nav.classList.contains('menu-active')){
+        nav.style.transition='height 0.4s, transform 0.4s, opacity 0.3s';
+     
+        nav.style.opacity='0';
+        
+        setTimeout(() => {
+            document.body.style.overflow='auto';
+            nav.classList.remove('menu-active');
+            nav.style.transition=''
+        }, 500);
+
+    }else{
+        nav.classList.add('menu-active');
+        document.body.style.overflow='hidden'
+
+        requestAnimationFrame(()=>{
+            nav.style.transition='height 0.4, transform 0.4, opacity 0.4s'
+            nav.style.opacity='1'
+            nav.style.transform='translateY(0)'
+        })
+    }
 })
